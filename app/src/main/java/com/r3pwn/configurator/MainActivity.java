eb989fc1edd741c1c912557bd3582a12178fffb5
r3pwn <wickett06@gmail.com>
2015-03-11 14:47:19 -0400

    No comment. Well, actually, just a few.

--- app/src/main/java/com/r3pwn/configurator/MainActivity.java
+++ app/src/main/java/com/r3pwn/configurator/MainActivity.java
@@ -16,80 +16,11 @@ import java.sql.*;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		/**
-		Flags:
-		Google Play Music
-		---------------------------
-		 music_download_passthrough_cookies
-		 music_supported_audio
-		 music_chunk_size_bites
-		 music_throttle_download
-		 music_throttle_download_kbps
-		 music_throttle_download_kb
-		 music_enable_storage_migrations
-		 music_short_term_cache_min_free_space
-		 music_long_term_cache_min_free_space
-		 music_download_max_blacklist_size
-		 music_download_blacklist_ttl_sec
-		 music_local_http_stuffing
-		 music_shop_url
-		 music_enable_tracks_upsync_deletion
-		 music_action_bar_height
-		 music_notification_bar_height
-		 music_device_enable_active_check
-		 music_device_type
-		 music_smartphone_min_screen_size_in_tenth_of_inch
-		 music_smartphone_max_screen_size_in_tenth_of_inch
-		 music_use_star_ratings
-		 music_overdraw_listview
-		 music_using_new_download_ui
-		 music_temp_nautilus_status_exp_time_in_seconds
-		 music_enable_sync_notifications
-		 music_enable_autocache
-		 music_autocache_opt_in_default_nautilus
-		 music_autocache_opt_in_default
-		 music_enable_find_video
-		 music_enable_cast_remote_playback
-		 music_nautilus_offline_status_ttl_days
-		 music_show_radio_animation
-		 music_min_version_upgrade_sync
-		 music_downstream_page_size
-		 music_upstream_page_size
-		 music_sync_radio
-		 music_sync_config
-		 music_merge_block_size
-		 music_enable_track_stats_upsync
-		 music_periodic_sync_frequency_in_seconds
-		 music_max_ephemeral_top_size
-		 music_sync_invalid_account
-		 music_use_downstream_posts
-		 music_sync_server_options
-		 music_enable_playlist_radio
-		 music_enable_keepon_radio
-		 music_enable_secondary_sdcards
-		 music_debug_logs_enabled
-		 music_enable_shared_playlists
-		 music_gapless_enabled
-		 music_gapless_enabled_pinned
-		 music_enable_chromecast_wake_lock
-		 music_cast_app_name
-		 musoc_version_key
-		 music_keepon_radio_always_use_downloaded_songs
-		 
-		Google Play Movies
-		---------------------------
-		 videos:dogfood_enabled
-		*/
-		final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-
-		final boolean nevertellme = preferences.getBoolean("nevertellme", false);
-        final SharedPreferences.Editor prefs_edit = preferences.edit();
 		final Button configure = (Button)findViewById(R.id.mainButton1);
 		final EditText name = (EditText)findViewById(R.id.flagNameET);
 		final EditText value = (EditText)findViewById(R.id.flagValueET);
@@ -125,11 +56,15 @@ public class MainActivity extends Activity
 					{
 						String flagName = name.getText().toString();
 						String flagValue = value.getText().toString();
+						// Look at me. I'm the captain now.
 						Shell.SU.run("cp /data/data/com.google.android.gsf/databases/gservices.db /data/data/com.r3pwn.configurator/databases/gservices.db\n");
 						SQLiteDatabase db=openOrCreateDatabase("gservices.db", Context.MODE_WORLD_READABLE, null);
+						// All your overrides are belong to me.
 						db.execSQL("INSERT INTO overrides (name, value) VALUES ('" + flagName + "', '" + flagValue + "');");
 						db.execSQL("UPDATE overrides SET value='" + flagValue + "' WHERE name='" + flagName + "';");
+						// Just kidding. You can have it back now.
 						Shell.SU.run("cp /data/data/com.r3pwn.configurator/databases/gservices.db /data/data/com.google.android.gsf/databases/gservices.db\n");
+						Shell.SU.run("rm -f /data/data/com.r3pwn.configurator/databases/gservices.db\n");
 						Toast.makeText(getApplicationContext(), "Done. Please reboot.", Toast.LENGTH_LONG).show();
 					}
 					catch(android.database.SQLException sqle)
@@ -140,6 +75,7 @@ public class MainActivity extends Activity
 						SQLiteDatabase db=openOrCreateDatabase("gservices.db", Context.MODE_WORLD_READABLE, null);
 						db.execSQL("UPDATE overrides SET value='" + flagValue + "' WHERE name='" + flagName + "';");
 						Shell.SU.run("cp /data/data/com.r3pwn.configurator/databases/gservices.db /data/data/com.google.android.gsf/databases/gservices.db\n");
+						Shell.SU.run("rm -f /data/data/com.r3pwn.configurator/databases/gservices.db\n");
 						Toast.makeText(getApplicationContext(), "Done. Please reboot.", Toast.LENGTH_LONG).show();
 					}
 				}
@@ -180,6 +116,7 @@ public class MainActivity extends Activity
 					String process;
 					if (killet.getText().toString().matches(""))
 					{
+						// User is not very smart or clicked the button by accident.
 						final AlertDialog sualertDialog = new AlertDialog.Builder(MainActivity.this).create();
 						sualertDialog.setTitle("You need to enter a process name");
 						sualertDialog.setMessage("You forgot to enter a process name. It's okay, this step is optional.");
@@ -195,37 +132,17 @@ public class MainActivity extends Activity
 					{
 						try {
 							process = killet.getText().toString();
-							java.lang.Process su = Runtime.getRuntime().exec("su");
+							// This spawns a system shell, which has access to kill processes. Neat, right?
+							// Still not sure why the superuser can't kill processes on Lollipop. Not so super, now, are you, user?
+							java.lang.Process su = Runtime.getRuntime().exec("su - system");
 							DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
+							// Kill all the things.
 							outputStream.writeBytes("am force-stop com.google.android.gsf\n");
 							outputStream.writeBytes("am force-stop com.google.android.gms\n");
 							outputStream.writeBytes("am force-stop " + process + "\n");
 							outputStream.writeBytes("exit\n");
 							outputStream.flush();
 							su.waitFor();
-							if (Build.VERSION.SDK_INT >= 21)
-							{
-								final AlertDialog sualertDialog = new AlertDialog.Builder(MainActivity.this).create();
-								sualertDialog.setTitle("You are running Lollipop or higher");
-								sualertDialog.setMessage("This feature may not work. Go check. If it didn't work, then a reboot may be required before changes take effect.");
-								sualertDialog.setButton("Oh, okay.", new DialogInterface.OnClickListener() {
-										public void onClick(DialogInterface dialog, int which) 
-										{
-											sualertDialog.dismiss();
-										}
-									});
-								if (nevertellme == false)
-								{
-									sualertDialog.setButton2("Never tell me this again.", new DialogInterface.OnClickListener() {
-										public void onClick(DialogInterface dialog, int which) 
-										{
-											prefs_edit.putBoolean("nevertelleme", true);
-											prefs_edit.commit();
-										}
-									});
-									sualertDialog.show();
-								}
-							}
 						}
 						catch(IOException e)
 						{
